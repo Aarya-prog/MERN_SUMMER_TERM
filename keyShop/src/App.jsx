1b9231a6f644a265cdc6c298fda9e0ff14ea9337
Aarya-prog <aaryakul2004@gmail.com>
2026-06-20 20:05:02 +0530

    problem 1 completed

---
+++ keyShop/src/App.jsx
@@ -0,0 +1,11 @@
+function App(){
+  return (
+    <div className="app">
+      <h1>Welcome to My React App</h1>
+      <p>This is a demo React application.</p>
+      <button>Click Me</button>
+    </div>
+  );
+}
+//JSX
+export default App;
